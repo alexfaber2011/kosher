dcf9a72e4c446bb413444dd60670ab6e1692f7be
alexfaber2011 <alexfaber2011@gmail.com>
2014-05-07 14:04:21 -0500

    Completed custom testing

--- free1.c
+++ free1.c
@@ -4,10 +4,10 @@
 #include "mem.h"
 
 int main() {
-   int x = 50;   //temporary variable to point to
-   void* ptr[4];
-   void* badPtr;
-   badPtr = &x;
+   int x = 50;                   //temporary variable to point to
+   void* ptr[4];                 
+   void* badPtr;                 //Will be as its name suggets, our bad pointer
+   badPtr = &x;                  //gives badPtr an address (of x) that resides outside of what Mem_Init set aside for us
 
    assert(Mem_Init(4096) == 0);
 
@@ -16,16 +16,18 @@ int main() {
 
    //Free it; test for 0 because it should've worked fine
    assert(Mem_Free(ptr[0]) == 0);
-   puts("After assert(Mem_Free(ptr[0]) == 0);");
-   Mem_Dump();
 
    //Free it again; should return -1 because the pointer has already been freed
    assert(Mem_Free(ptr[0]) == -1);
-   puts("Mem_Free(ptr[0]) == 0) Again");
-   Mem_Dump();
+
+   //Give it a pointer that resides outside of the address space that Mem_Init allocated for us
+   // it will return a negative -1 because that pointer won't exist in our linked list, so 
+   // check for it
    assert(Mem_Free(badPtr) == -1);
-   puts("After 'freeing' pointer that resides outside of address space");
-   Mem_Dump();
+
+   //Check to see if Mem_Free handles a NULL pointer properly
+   badPtr = NULL;
+   assert(Mem_Free(badPtr) == -1);
 
    exit(0);
 }
